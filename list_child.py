289785a5985ded0579aaa9fc738822833c0c6ab8
Paul <p.hermouet@catie.fr>
2018-05-22 13:50:05 +0200

    append -> pack

--- list_child.py
+++ list_child.py
@@ -1,12 +1,12 @@
 class MyList(list):
-	def append(self, obj):
+	def pack(self, obj):
 		assert(isinstance(obj, dict) and "source" in obj)
 		if len(self):
 			last = self[-1]
 			n = len(last)
 			if n  == 3:
-				super().append({"src0": obj["source"]})
+				self.append({"src0": obj["source"]})
 			else:
 				last["src{}".format(n)] = obj["source"]
 		else:
-			super().append({"src0": obj["source"]})
+			self.append({"src0": obj["source"]})
